f2feba55f061974ea458a181f8b6ee323b06378f
Andrew Lake <jamboarder@gmail.com>
2009-10-27 18:45:59 -0700

    Geeez... forgot to add actionmanager files... I'm a dufus...

---
+++ actionsmanager.h
@@ -0,0 +1,70 @@
+/* BANGARANG MEDIA PLAYER
+* Copyright (C) 2009 Andrew Lake (jamboarder@yahoo.com)
+* <http://gitorious.org/bangarang>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef ACTIONSMANAGER_H
+#define ACTIONSMANAGER_H
+
+#include <QObject>
+#include <QAction>
+
+namespace Ui
+{
+    class MainWindowClass;
+}
+class MainWindow;
+
+class ActionsManager : public QObject
+{
+    Q_OBJECT
+    
+    public:
+        ActionsManager(MainWindow * parent);
+        ~ActionsManager();
+        
+        QAction *quit();
+        QAction *playAll();
+        QAction *playSelected();
+        QAction *addSelectedToPlaylist();
+        QAction *removeSelectedFromPlaylist();
+        QAction *fullScreen();
+        QAction *showHideControls();
+        QAction *cancelFullScreenHideControls();
+        
+    public slots:
+        
+    private:
+        MainWindow *m_parent; 
+        Ui::MainWindowClass *ui;
+        
+        QAction *m_quit;
+        QAction *m_playAllAction;
+        QAction *m_playSelectedAction;
+        QAction *m_addSelectedToPlayListAction;
+        QAction *m_removeSelectedToPlayListAction;
+        QAction *m_showHideControls;
+        QAction *m_fullScreen;
+        QAction *m_cancelFullScreenHideControls;
+        
+    private slots:
+        void fullScreenToggle();
+        void toggleControls();
+        void cancelFSHC();
+        
+
+};
+#endif //ACTIONSMANAGER_H
