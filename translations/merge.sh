4d161bf11e5caaf4050ec89c4c8671bb1256526b
Łukasz Jernaś <deejay1@srem.org>
2010-01-14 09:47:06 +0100

    Keep the translation list sorted

--- translations/merge.sh
+++ translations/merge.sh
@@ -1,7 +1,7 @@
 #! /bin/bash 
 #Add your translation here to have it merged.
 
-msgmerge -U nl/bangarang.po bangarang.pot
 msgmerge -U de/bangarang.po bangarang.pot
 msgmerge -U fr/bangarang.po bangarang.pot
+msgmerge -U nl/bangarang.po bangarang.pot
 msgmerge -U zh_CN/bangarang.po bangarang.pot
