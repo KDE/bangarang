20fa60d8010a95b109598b56643fc897a837a767
Andrew Lake <jamboarder@yahoo.com>
2010-01-25 20:07:29 -0800

    merge Czech and Brazillian Portuguese translations

--- translations/merge.sh
+++ translations/merge.sh
@@ -1,7 +1,9 @@
 #! /bin/bash 
 #Add your translation here to have it merged.
 
+msgmerge -U cz/bangarang.po bangarang.pot
 msgmerge -U de/bangarang.po bangarang.pot
 msgmerge -U fr/bangarang.po bangarang.pot
 msgmerge -U nl/bangarang.po bangarang.pot
+msgmerge -U pt_BR/bangarang.po bangarang.pot
 msgmerge -U zh_CN/bangarang.po bangarang.pot
