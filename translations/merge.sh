9120a337e8e7c22d3d2f7a80e6c0478aeb4f6d9f
Andrew Lake <jamboarder@gmail.com>
2010-03-28 17:30:33 -0700

    update messages.

--- translations/merge.sh
+++ translations/merge.sh
@@ -4,6 +4,7 @@
 msgmerge -U cz/bangarang.po bangarang.pot
 msgmerge -U de/bangarang.po bangarang.pot
 msgmerge -U fr/bangarang.po bangarang.pot
+msgmerge -U it/bangarang.po bangarang.pot
 msgmerge -U nl/bangarang.po bangarang.pot
 msgmerge -U pl/bangarang.po bangarang.pot
 msgmerge -U pt_BR/bangarang.po bangarang.pot
