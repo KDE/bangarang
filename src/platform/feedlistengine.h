08e56af788b53b3a4b1383effcf9943474c2b7f6
Andrew Lake <jamboarder@gmail.com>
2010-06-27 10:28:53 -0700

    add support for RSS 2.0 media feed tags and preferential tag use.

--- src/platform/feedlistengine.h
+++ src/platform/feedlistengine.h
@@ -25,6 +25,8 @@
 #include <KUrl>
 #include <kjob.h>
 #include <kio/copyjob.h>
+#include <QDomDocument>
+#include <QDomNodeList>
 
 class MediaItem;
 class MediaListProperties;
@@ -45,6 +47,8 @@ class FeedListEngine : public NepomukListEngine
         
     private:
         QString m_feedFilePath;
+
+        QDomElement getPreferredTag(const QDomNodeList &itemNodes, const QStringList &tagPref);
         
     private slots:
         void downloadComplete(const KUrl &from, const KUrl &to);
