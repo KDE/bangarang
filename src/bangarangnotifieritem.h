64738449e3f0229957b66273c1b2dd791e691be1
Steve Fischer <fischenderHobbit@web.de>
2010-07-02 12:54:26 +0200

    Add an improved tray icon (based on KStatusNotifierItem)
    The improved version supports reaction on some common actions (e.g. a middle click will do the
    same as the pause/play action from the contextmenu).
    The icon of the app provides also some feedback about the current state (muted, play/pause,
    current position in track...).

---
+++ src/bangarangnotifieritem.h
@@ -0,0 +1,104 @@
+/* BANGARANG MEDIA PLAYER
+* Copyright (C) 2009 Andrew Lake (jamboarder@yahoo.com)
+* <http://gitorious.org/bangarang>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef BANGARANGNOTIFIERITEM_H
+#define BANGARANGNOTIFIERITEM_H
+
+#include <QIcon>
+#include <phonon/phononnamespace.h>
+
+#include <KStatusNotifierItem>
+
+/**
+* Bangarang KStatusNotifierIcon.
+*
+* Represents the app in the systray.
+*
+* The icon will 'expire' with increased playing time of an track.
+* If Phonon::State is playing or paused a respective overlay icon is shown.
+* If the player is muted this will be shown by a overlayicon, too.
+*
+* If the user performs an mouse scroll action on the icon, either a changeTrackRequest or
+* changeVolumeRequest is emitted.
+* If the icon recognizes a middle click the changeStateRequest will be emitted.
+*
+* @author Steve Fischer <fischenderHobbit@web.de>
+*/
+class BangarangNotifierItem : public KStatusNotifierItem
+{
+  Q_OBJECT
+    
+  public:
+    BangarangNotifierItem(QObject* parent = 0);
+    virtual ~BangarangNotifierItem();
+    
+    Phonon::State state() const;
+    void setState(Phonon::State state);
+    
+    bool isVolumeMuted() const;
+    void setVolumeMuted(bool muted);
+    
+  signals:
+    /**
+    * Inform the associated window that the state should changed either from playing to paused or 
+    * from paused to playing.
+    * @param newState either 'Playing' or 'Paused'
+    */
+    void changeStateRequested(Phonon::State newState);
+    
+    /**
+    * Inform the associated window that the current track should be changed.
+    * @param steps steps back/forward in playlist
+    */
+    void changeTrackRequested(int steps);
+    
+    /**
+    * Inform the associated window that the volume should be increased or decreased.
+    * @param delta increase/decrease value
+    */
+    void changeVolumeRequested(int delta);
+  
+  public slots:
+    /**
+    * Perform an icon update.
+    * The icon will 'expire' (fade to grey) by increased playing time.
+    *
+    * @param position current playing position
+    * @param length current track length
+    */
+    void updateAppIcon(qint64 position, qint64 length);
+    
+  private:
+    void updateOverlayIcon();
+    void updateIcon();
+    
+    Phonon::State m_currentState;
+    bool m_muted;
+    
+    QPixmap m_icon;
+    QPixmap m_grayIcon;
+    
+    int m_iconSize;
+    int m_pos;
+    
+  private slots:
+    void handleMiddleClick();
+    void handleScrollRequested(int delta, Qt::Orientation orientation);
+};
+
+#endif // BANGARANGNOTIFIERITEM_H
