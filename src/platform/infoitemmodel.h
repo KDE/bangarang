2b925ed14c6ec14e779f615bea76fb4d48e264da
Andrew Lake <jamboarder@gmail.com>
2010-10-31 14:04:13 -0700

    code cleanup: provide some documentation for InfoItemModel.

--- src/platform/infoitemmodel.h
+++ src/platform/infoitemmodel.h
@@ -28,13 +28,18 @@ class InfoFetcher;
 class MediaIndexer;
 
 /*
-* This model is responsible for 
-*
+* This model is responsible for storing information contained in a MediaItem or list of MediaItems.
+* It recognizes shared values across media items and provides InfoFetchers for updating MediaItem
+* information.  It is useful for displaying and editing MediaItem information.  If a MediaItemModel
+* is provided it can request changes to be saved to the source of the MediaItem information.
 */ 
 class InfoItemModel : public QStandardItemModel
 {
     Q_OBJECT
     public:
+        /**
+          * Roles for storing and accessing data stored by this model.
+          **/
         enum InfoItemRoles {FieldRole = Qt::UserRole,
                             FieldNameRole = Qt::UserRole + 1,
                             MultipleValuesRole = Qt::UserRole + 2,
@@ -42,23 +47,108 @@ class InfoItemModel : public QStandardItemModel
                             ValueListRole = Qt::UserRole + 4,
                             ArtworkListRole = Qt::UserRole + 5,
                             DrillRole = Qt::UserRole + 6};
+
+        /**
+          * Type of fetch.
+          **/
         enum FetchType {AutoFetch = 0,
                         Fetch = 1};
+
+        /**
+          * Constructor
+          **/
         InfoItemModel(QObject * parent = 0);
+
+        /**
+          * Destructor
+          **/
         ~InfoItemModel();
+
+        /**
+          * Load information into the model for the provided list of MediaItems
+          **/
         void loadInfo(const QList<MediaItem> & mediaList);
+
+        /**
+          * Save changes made to the data contained in the model
+          **/
         void saveChanges();
+
+        /**
+          * Cancel changes made to the data contained in the model
+          **/
         void cancelChanges();
+
+        /**
+          * Returns the list of MediaItems for which information is stored in the model
+          **/
         QList<MediaItem> mediaList();
+
+        /**
+          * Sets the MediaItemModel which generated the MediaItems provide to this model.
+          * This is the model that is used to save changes made to the information for
+          * provided MediaItems
+          **/
         void setSourceModel(MediaItemModel *sourceModel);
+
+        /**
+          * Returns list of InfoFetchers provided by this model
+          **/
         QList<InfoFetcher *> infoFetchers();
+
+        /**
+          * Returns list of InfoFetchers that are available for fetching info for the
+          * currently load MediaItems.
+          **/
         QList<InfoFetcher *> availableInfoFetchers();
+
+        /**
+          * Returns true if provided InfoFetcher can automatically fetch information for the currently
+          * loaded MediaItems.
+          **/
         bool autoFetchIsAvailable(InfoFetcher* infoFetcher);
+
+        /**
+          * Returns true if provided InfoFetcher can fetch information for the currently
+          * loaded MediaItems.
+          **/
         bool fetchIsAvailable(InfoFetcher* infoFetcher);
+
+        /**
+          * Autofetch information for the currently loaded MediaItems.  Changes
+          * will automatically be saved.
+          **/
         void autoFetch(InfoFetcher* infoFetcher, bool updateRequiredFields = true, bool updateArtwork = true);
+
+        /**
+          * Fetch information for the currently loaded MediaItems. Fetched information will
+          * loaded into model as changes - changes are not automatically saved.
+          **/
         void fetch(InfoFetcher* infoFetcher);
+
+        /**
+          * Set the rating for all currently loaded MediaItems. Rating changes are automatically saved.
+          **/
+
         void setRating(int rating);
                    
+    Q_SIGNALS:
+        /**
+          * Emitted when information contained in the model might have been changed.
+          **/
+        void infoChanged(bool modified);
+
+        /**
+          * Emitted when information is being fetched for MediaItems contained in the model.
+          **/
+        void fetching();
+
+        /**
+          * Emitted when information fetching has completed. Note that this signal is
+          * also emitted if fetching times out.
+          **/
+        void fetchComplete();
+
     private:
         QList<MediaItem> m_mediaList;
         MediaItemModel * m_sourceModel;
@@ -83,11 +173,6 @@ class InfoItemModel : public QStandardItemModel
         QString categoryTypeForField(const QString &field, const QString &type);
         MediaItem createDrillItem(const QString &field, const QString &type, const QString &value);
 
-    Q_SIGNALS:
-        void infoChanged(bool modified);
-        void fetching();
-        void fetchComplete();
-        
     private Q_SLOTS:
         void itemChanged(QStandardItem *changedItem);
         void infoFetched(MediaItem mediaItem);
