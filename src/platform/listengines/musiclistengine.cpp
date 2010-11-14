42d6c78628144944325a86679b86839c16c71274
Andrew Lake <jamboarder@gmail.com>
2010-11-14 14:40:20 -0800

    fix broken caching of songs results - make sure last results are emitted and marked as done.

--- src/platform/listengines/musiclistengine.cpp
+++ src/platform/listengines/musiclistengine.cpp
@@ -384,6 +384,8 @@ void MusicListEngine::run()
                     mediaList.clear();
                     offset += limit;
                     query.addOffset(offset);
+                } else {
+                    emit results(m_requestSignature, mediaList, m_mediaListProperties, true, m_subRequestSignature);
                 }
             }
         } 
