e4bdcf4cb8165f9c82fb4dd159f6686c1401340d
Andrew Lake <jamboarder@gmail.com>
2011-01-23 10:20:46 -0800

    Show artwork for series and tv episodes in media list.

--- src/mediaitemdelegate.cpp
+++ src/mediaitemdelegate.cpp
@@ -167,7 +167,8 @@ void MediaItemDelegate::paint(QPainter *painter, const QStyleOptionViewItem &opt
         } else {
             QPixmap artwork;
             if (subType == "Artist" || subType == "Album" || subType == "AudioGenre" || subType =="Audio Stream" || subType == "Audio Feed" ||
-                subType == "Movie" || subType == "VideoGenre" || subType == "Actor" || subType == "Director" || subType == "Video Feed") {
+                subType == "Movie" || subType == "VideoGenre" || subType == "Actor" || subType == "Director" || subType == "Video Feed" ||
+                subType == "TV Series" || subType == "TV Show") {
                 if (Utilities::artworkIsInCache(mediaItem)) {
                     artwork = Utilities::getArtworkFromMediaItem(mediaItem);
                 } else if (artworkNeededIndex(mediaItem) == -1) {
