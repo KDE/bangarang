31810af3467f3fc7122f0e6ca96431f9e752f158
Kristof Bal <kristof.bal@gmail.com>
2011-09-11 12:24:42 +0200

    Adapted Messages.sh
    Messages.sh now works again in the new source structure. Also remove merge.sh, since this is done by Messages.sh now. Of course this is a much cleaner and easier
    solution.

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,14 +1,28 @@
-#! /bin/bash 
-#Script to extract translateble messages from the code.
-#The following part is used to do the same as Scripty in KDE svn
+#!/bin/bash 
+# Script to extract translatable messages from the code.
 
-XGETTEXT="xgettext --copyright-holder=This_file_is_part_of_KDE --from-code=UTF-8 -C --kde -ci18n -ki18n:1 -ki18nc:1c,2 -ki18np:1,2 -ki18ncp:1c,2,3 -ktr2i18n:1 -kI18N_NOOP:1 -kI18N_NOOP2:1c,2 -kaliasLocale -kki18n:1 -kki18nc:1c,2 -kki18np:1,2 -kki18ncp:1c,2,3 --msgid-bugs-address=http://bugs.kde.org"
+# Work usually done by scripty in KDE svn
+XGETTEXT="xgettext --copyright-holder=This_file_is_part_of_KDE --from-code=UTF-8 -C --kde -ci18n -ki18n:1 -ki18nc:1c,2 -ki18np:1,2 -ki18ncp:1c,2,3 -ktr2i18n:1 -kI18N_NOOP:1 -kI18N_NOOP2:1c,2 -kaliasLocale -kki18n:1 -kki18nc:1c,2 -kki18np:1,2 -kki18ncp:1c,2,3 --msgid-bugs-address=http://code.google.com/p/bangarangissuetracking/issues"
 EXTRACTRC="extractrc"
 export XGETTEXT EXTRACTRC
 
 echo 'i18nc("NAME OF TRANSLATORS","Your names");' >> rc.cpp
-echo 'i18nc("EMAIL OF TRANSLATORS","Your emails");' >> rc.cpp 
+echo 'i18nc("EMAIL OF TRANSLATORS","Your emails");' >> rc.cpp
+ 
+# Extract
+echo 'Extracting messages...'
+$EXTRACTRC ui/*.ui >> rc.cpp
+$XGETTEXT *.cpp platform/*.cpp platform/infofetchers/*.cpp platform/listengines/*.cpp platform/utilities/*.cpp -o ../po/bangarang.pot
 
-$EXTRACTRC *.ui >> rc.cpp
-$XGETTEXT *.cpp platform/*.cpp platform/infofetchers/*.cpp platform/listengines/*.cpp platform/utilities/*.cpp platform/utilities/*.cpp -o ../po/bangarang.pot
+# Merge
+echo 'Merging messages...'
+for lang in $( find ../po  -maxdepth 1 -mindepth 1 -type d )
+do
+     msgmerge -U $lang/bangarang.po ../po/bangarang.pot 
+done
+
+# Clean
+echo 'Cleaning up...'
 rm -f rc.cpp
+
+echo 'Done!'
